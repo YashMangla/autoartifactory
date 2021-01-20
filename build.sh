e785de1678562dfc4dc4917ba84097c5f533660c
YashMangla <ymangla37@gmail.com>
2021-01-20 22:10:41 +0530

    10th comit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+install_dependencies(){
+	apt-get -y update
+	apt-get install rsync -y
+	apt-get install git -y
+	apt-get install curl -y
+	apt-get install iputils-ping -y
+}
+
+commands_execute(){
+	ls
+	pwd
+	ping 192.168.1.6
+}
+
+
